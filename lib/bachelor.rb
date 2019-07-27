af8aaa2b7201eddc341db772493d7cdf98fb588c
Tracie Masek <traciemasek@gmail.com>
2019-07-27 20:55:56 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,12 +2,7 @@ require 'pry'
 
 def get_first_name_of_season_winner(data, season)
   data[season].each do |hash|
-    #binding.pry
-    hash.each do |detail, info|
-      if detail == "status"
-        if 
-      end
-    end
+    
   end
 end
 
