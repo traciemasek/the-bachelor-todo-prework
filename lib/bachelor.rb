043355be220f9f3d9dc381f4e6088972009d4e84
Tracie Masek <traciemasek@gmail.com>
2019-07-27 21:20:46 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -9,7 +9,8 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestant_hash|
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
