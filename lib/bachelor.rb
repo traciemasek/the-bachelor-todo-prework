8149fcf71b5028fbeec90f9b741f3b30aa8a2dd0
Tracie Masek <traciemasek@gmail.com>
2019-07-27 20:41:16 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,6 +3,7 @@ require 'pry'
 def get_first_name_of_season_winner(data, season)
   data[season].each do |key, value|
     if key["status"] == "Winner"
+      key["name"].split(" ")[0]
   end
   
 end
