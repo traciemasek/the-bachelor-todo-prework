2655538fdb12f3bb833a9b0e25d1e325561192e2
Tracie Masek <traciemasek@gmail.com>
2019-07-27 20:35:25 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,6 +3,8 @@ require 'pry'
 def get_first_name_of_season_winner(data, season)
   data.each do |season_num, contestants|
     if season_num == season
+      contestants.each do ||
+      end
     end
   end
 end
