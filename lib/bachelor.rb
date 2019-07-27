a550d891b23ca5a30525bff058273ceaff21712b
Tracie Masek <traciemasek@gmail.com>
2019-07-27 21:28:06 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -19,7 +19,15 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  hometown_count = 0
+  hometown_count = 0 
+  data.each do |season, hash|
+    hash.each do |details|
+      if details["hometown"] == hometown
+        return details["name"]
+      end
+    end
+  end
+  
 end
 
 def get_occupation(data, hometown)
